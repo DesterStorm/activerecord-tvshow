c205c166229d484289e4c088f7f0fe0088b69fb7
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-09 16:26:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,10 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
